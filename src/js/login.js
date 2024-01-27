55a7d448e86e8207d5c0ae07257d983dc693ff50
Gui Henrry <guilhermehenry60@gmail.com>
2024-01-27 10:44:36 -0300

    feat: change login page to use class

--- src/js/login.js
+++ src/js/login.js
@@ -1,33 +1,65 @@
 import Toast from 'bootstrap/js/dist/toast'
+
 import supabase from './services/supabase'
 
-const formLogin = document.getElementById('formLogin')
-const buttonSubmit = document.getElementById('buttonSubmit')
-const toastErrorLogin = document.getElementById('toastErrorLogin')
-const toast = Toast.getOrCreateInstance(toastErrorLogin)
+class LoginPage {
+  setLoadingState(isLoading) {
+    const buttonSubmit = this.$formLogin.querySelector('[type="submit"]')
+    buttonSubmit.disabled = isLoading
+    buttonSubmit.innerText = isLoading ? 'Carregando...' : 'Entrar'
+  }
 
-function setLoadingState(isLoading) {
-  buttonSubmit.disabled = isLoading
-  buttonSubmit.innerText = isLoading ? 'Carregando...' : 'Entrar'
-}
+  async login(credentials) {
+    this.setLoadingState(true)
+    const response = await supabase.auth.signInWithPassword(credentials)
+    this.setLoadingState(false)
+    return response
+  }
 
-async function handleFormSubmit(event) {
-  event.preventDefault()
-  if (!event.target.checkValidity()) return
-
-  const email = event.target.elements['email']?.value
-  const password = event.target.elements['password']?.value
-
-  setLoadingState(true)
-  const { error } = await supabase.auth.signInWithPassword({ email, password })
-  setLoadingState(false)
-
-  if (error) {
-    console.error(error)
-    toast.show()
-  } else {
+  redirectToMoviesPage() {
     window.location.href = 'index.html'
+  }
+
+  async handleFormSubmit(event) {
+    event.preventDefault()
+
+    const credentials = {
+      email: event.target.elements['email']?.value,
+      password: event.target.elements['password']?.value,
+    }
+
+    const { error } = await this.login(credentials)
+
+    if (error) {
+      console.error(error)
+      this.toast.show()
+    } else {
+      this.redirectToMoviesPage()
+    }
+  }
+
+  bindEvents() {
+    this.handleFormSubmit = this.handleFormSubmit.bind(this)
+  }
+
+  addEvents() {
+    this.$formLogin.addEventListener('submit', this.handleFormSubmit)
+  }
+
+  init() {
+    this.bindEvents()
+    this.addEvents()
+  }
+
+  constructor({ toast, $formLogin }) {
+    this.toast = toast
+    this.$formLogin = $formLogin
   }
 }
 
-formLogin.addEventListener('submit', handleFormSubmit)
+const loginPage = new LoginPage({
+  toast: Toast.getOrCreateInstance(document.getElementById('toastErrorLogin')),
+  $formLogin: document.getElementById('formLogin'),
+})
+
+loginPage.init()
